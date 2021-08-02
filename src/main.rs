f6f9d055a31a223ad8fe6383cd272944defe06d7
Boni Lindsley <boni.lindsley@gmail.com>
2021-08-02 13:22:31 +0100

    Move ttspico calls into own impl

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,36 @@
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use ttspico as pico;
+
+type ExitCode = i32;
+type RunResult = Result<(), Box<dyn std::error::Error>>;
 
 fn main() {
-    // 1. Create a Pico system
-    // NOTE: There should at most one System per thread!
-    let sys = pico::System::new(4 * 1024 * 1024)
-        .expect("Could not init system");
+    let run_result = run();
+    let exit_code = parse_run_result(run_result);
+    std::process::exit(exit_code);
+}
 
-    // 2. Load Text Analysis (TA) and Speech Generation (SG) resources
-    // for the voice you want to use
-    let ta_res = pico::System::load_resource(
-        std::rc::Rc::clone(&sys),
-        "/usr/share/pico/lang/en-GB_ta.bin",
-    )
-    .expect("Failed to load TA");
-    let sg_res = pico::System::load_resource(
-        std::rc::Rc::clone(&sys),
-        "/usr/share/pico/lang/en-GB_kh0_sg.bin",
-    )
-    .expect("Failed to load SG");
-    println!(
-        "TA: {}, SG: {}",
-        ta_res.borrow().name().unwrap(),
-        sg_res.borrow().name().unwrap()
-    );
-
-    // 3. Create a Pico voice definition
-    // and attach the loaded resources to it
-    let voice = pico::System::create_voice(sys, "TestVoice")
-        .expect("Failed to create voice");
-    voice
-        .borrow_mut()
-        .add_resource(ta_res)
-        .expect("Failed to add TA to voice");
-    voice
-        .borrow_mut()
-        .add_resource(sg_res)
-        .expect("Failed to add SG to voice");
-
-    // 4. Create an engine from the voice definition
-    // UNSAFE: Creating an engine without attaching the resources
-    // will result in a crash!
-    let mut engine = unsafe {
-        pico::Voice::create_engine(voice)
-            .expect("Failed to create engine")
-    };
-
-    // 5. Put (UTF-8) text to be spoken into the engine
-    // See `Engine::put_text()` for more details.
-    // The null terminator tells Pico to start synthesizing.
-    let mut text_bytes: &[u8] = b"1, 2, 3, Hello Rust!\0";
-    while text_bytes.len() > 0 {
-        let n_put = engine
-            .put_text(text_bytes)
-            .expect("pico_putTextUtf8 failed");
-        text_bytes = &text_bytes[n_put..];
-    }
-
-    // 6. Do the actual text-to-speech,
-    // getting audio data (16-bit signed PCM @ 16kHz) from the input text
-    // Speech audio is computed in small chunks, one "step" at a time;
-    // see `Engine::get_data()` for more details.
-    let mut pcm_data: Vec<i16> = vec![0i16; 0];
-    let mut pcm_buf = [0i16; 1024];
-    'tts: loop {
-        let (n_written, status) = engine
-            .get_data(&mut pcm_buf[..])
-            .expect("pico_getData error");
-        pcm_data.extend(&pcm_buf[..n_written]);
-        if status == ttspico::EngineStatus::Idle {
-            break 'tts;
+fn parse_run_result(result: RunResult) -> ExitCode {
+    match result {
+        Ok(()) => 0,
+        Err(error) => {
+            eprintln!("{}", error);
+            1
         }
     }
+}
 
+fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let memory_size = 4 * 1024 * 1024;
+    let text_analysis_data_path = "/usr/share/pico/lang/en-GB_ta.bin";
+    let speech_generation_data_pata =
+        "/usr/share/pico/lang/en-GB_kh0_sg.bin";
+    let mut ttspico_backend = TTSPicoBackend::new(
+        memory_size,
+        text_analysis_data_path,
+        speech_generation_data_pata,
+    )?;
+    let source = b"1, 2, 3, Hello Rust!\0";
+    let pcm_data = ttspico_backend.generate(source)?;
     // Plays an audio buffer (16-bit signed PCM @ 16kHz)
     // to the system default output device.
     // Exits the current process when done.
@@ -88,11 +46,10 @@ fn main() {
     let mut buffer = pcm_data.into_iter();
     let data_sender =
         move |data: &mut cpal::Data, _: &cpal::OutputCallbackInfo| {
-            data.as_slice_mut().unwrap().iter_mut().for_each(|d| {
-                *d = buffer //output
-                    .next() /*map(|s| s.to_i16()).*/
-                    .unwrap_or(0i16)
-            })
+            data.as_slice_mut()
+                .unwrap()
+                .iter_mut()
+                .for_each(|d| *d = buffer.next().unwrap_or(0i16))
         };
     let error_handler = |err| {
         eprintln!("Failed to output audio stream.\n{}", err);
@@ -107,4 +64,88 @@ fn main() {
         .expect("Failed to build output stream.");
     stream.play().expect("Failed to play output stream.");
     std::thread::sleep(std::time::Duration::from_millis(5000));
+    Ok(())
+}
+
+pub struct TTSPicoError {
+    description: String,
+}
+impl From<ttspico::PicoError> for TTSPicoError {
+    fn from(source: ttspico::PicoError) -> TTSPicoError {
+        let description = source.descr;
+        TTSPicoError { description }
+    }
+}
+impl std::error::Error for TTSPicoError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
+impl std::fmt::Debug for TTSPicoError {
+    fn fmt(
+        &self,
+        formatter: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
+        formatter
+            .debug_struct("TTSPicoError")
+            .field("description", &self.description)
+            .finish()
+    }
+}
+impl std::fmt::Display for TTSPicoError {
+    fn fmt(
+        &self,
+        formatter: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
+        write!(formatter, "TTS Pico error: {}", self.description)
+    }
+}
+
+pub struct TTSPicoBackend {
+    engine: ttspico::Engine,
+}
+impl TTSPicoBackend {
+    pub fn new(
+        memory_size: usize,
+        text_analysis_data_path: &str,
+        speech_generation_data_path: &str,
+    ) -> Result<TTSPicoBackend, TTSPicoError> {
+        let system = ttspico::System::new(memory_size)?;
+        let text_analysis_data = ttspico::System::load_resource(
+            std::rc::Rc::clone(&system),
+            text_analysis_data_path,
+        )?;
+        let speech_generation_data = ttspico::System::load_resource(
+            std::rc::Rc::clone(&system),
+            speech_generation_data_path,
+        )?;
+        let voice = ttspico::System::create_voice(system, "TestVoice")?;
+        voice.borrow_mut().add_resource(text_analysis_data)?;
+        voice.borrow_mut().add_resource(speech_generation_data)?;
+        let engine = unsafe { ttspico::Voice::create_engine(voice)? };
+        Ok(TTSPicoBackend { engine })
+    }
+
+    pub fn generate(
+        &mut self,
+        source: &[u8],
+    ) -> Result<Vec<i16>, TTSPicoError> {
+        let mut remaining = source;
+        while remaining.len() > 0 {
+            let n_put = self.engine.put_text(remaining)?;
+            remaining = &remaining[n_put..];
+        }
+        // 16-bit signed 16kHz PCM.
+        let mut pcm_data: Vec<i16> = vec![0i16; 0];
+        let mut pcm_buf = [0i16; 1024];
+        loop {
+            let (n_written, status) =
+                self.engine.get_data(&mut pcm_buf[..])?;
+            pcm_data.extend(&pcm_buf[..n_written]);
+            if status == ttspico::EngineStatus::Idle {
+                break;
+            }
+        }
+        Ok(pcm_data)
+    }
 }
